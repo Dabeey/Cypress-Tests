bdb8e9448dd824d69f709f4de4dcb84ed84e41f4
Dabeey <abbasdaughter18@gmail.com>
2025-08-08 22:40:19 +0100

    Create Integration folder, test_home file.

---
+++ cypress/integration/test_home.js
@@ -0,0 +1,6 @@
+// Assignment: Scenario: You’re testing a news website. 
+//  - Write a Cypress test to: 
+//     -1. Open the-internet.herokuapp.com, 
+//     -2. Verify the page title. 
+//  - Save as cypress/integration/test_home.js. 
+//  - Run with npx cypress run. Push to GitHub.
