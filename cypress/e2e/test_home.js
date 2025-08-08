7867e1b9fd6ec91b6b459b8fa463f368cb6634dc
Dabeey <abbasdaughter18@gmail.com>
2025-08-08 23:17:22 +0100

    Change file path

---
+++ cypress/e2e/test_home.js
@@ -0,0 +1,16 @@
+// Assignment: Scenario: You’re testing a news website. 
+//  - Write a Cypress test to: 
+//     -1. Open the-internet.herokuapp.com, 
+//     -2. Verify the page title. 
+//  - Save as cypress/integration/test_home.js. 
+//  - Run with npx cypress run. Push to GitHub.
+
+describe(' First test as a news website tester.', () => {
+    it('Open website and verify title', () => {
+        cy.visit(URL);
+        cy.title().should('include', 'The Internet');
+        // You can also use cy.title().should('eq', 'The Internet') if you
+        // want to check for an exact match.
+    } )
+
+})
