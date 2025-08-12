9a77acc36684ec6c360b63dcd27b882c9220d84f
Dabeey <abbasdaughter18@gmail.com>
2025-08-12 14:08:20 +0100

    Force default to Electron

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    pageLoadTimeout: 120000,
-    setupNodeEvents(on, config) {
-      // plugins/events
-    },
-    // baseUrl removed since we don't have a local server
-    supportFile: "cypress/support/e2e.js",
+    baseUrl: "http://localhost:3000",
   },
+  browser: "electron",
+  video: false,
+  screenshotOnRunFailure: false,
 });
